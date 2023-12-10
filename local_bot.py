408300559833eb1af7c23fce5a3152e61e0d06c5
Ilya M <ilyailiilia@gmail.com>
2023-12-10 04:46:44 +0100

    Handle null prompts

--- local_bot.py
+++ local_bot.py
@@ -1,16 +1,48 @@
 import re
 import nltk
-from datetime import datetime, time
+from datetime import datetime
 from transformers import pipeline
 
 
-def get_last_sentence(text):
+def run_model(model_name, prompt, rounds):
+    generator = pipeline("text-generation", model=model_name)
+    with open("out.txt", "a+") as f:
+        f.write("\n\n\n")
+        f.write("Model: " + model_name + "\n")
+        f.write("Time: " + str(datetime.now()) + "\n")
+        f.write("\n\n\n")
+        for _ in range(rounds):
+            prompt = _do_run_and_get_next_prompt(f, generator, prompt)
+        f.write("\n\n\n")
+
+
+def _do_run_and_get_next_prompt(f, generator, prompt):
+    f.write("Prompt: " + prompt + "\n")
+    out = generator(
+        prompt,
+        do_sample=True,
+        min_length=20,
+        max_new_tokens=100,
+    )
+    out = out[0]["generated_text"]
+    if out.startswith(prompt):
+        print("Prompt: " + prompt)
+        out = out[len(prompt) :]
+    print(out)
+    f.write(out + "\n")
+
+    return _get_last_sentence(out)
+
+
+def _get_last_sentence(text):
     # Ensure NLTK sentence tokenizer is downloaded
     try:
         nltk.data.find("tokenizers/punkt")
     except LookupError:
         nltk.download("punkt")
 
+    if text is None:
+        return "On. Somehow on. Till nohow on."
     # Tokenize the text into sentences
     sentences = nltk.tokenize.sent_tokenize(text)
 
@@ -21,48 +53,20 @@ def get_last_sentence(text):
             return sentence
 
     # If no sentence meets the condition, return None
-    return "On. Somehow on. Till nohow on."
-
-
-def run_model(model_name, prompt, rounds):
-    generator = pipeline("text-generation", model=model_name)
-
-    with open("out.txt", "a+") as f:
-        f.write("\n\n\n")
-        f.write("Model: " + model_name + "\n")
-        f.write("Time: " + str(datetime.now()) + "\n")
-        f.write("\n\n\n")
-        for i in range(rounds):
-            f.write("Prompt: " + prompt + "\n")
-            out = generator(
-                prompt,
-                do_sample=True,
-                min_length=20,
-                max_new_tokens=100,
-            )
-            out = out[0]["generated_text"]
-            # Delete prompt from output
-            if prompt in out:
-                print("Prompt: " + prompt)
-                out = out[len(prompt) :]
-            print(out)
-            f.write(out + "\n")
-
-            prompt = get_last_sentence(out)
+    return "Somehow on. Till nohow on. Said nohow on."
 
 
 if __name__ == "__main__":
     models = [
-        "ToddGoldfarb/Cadet-Tiny",
-        "KoboldAI/OPT-350M-Erebus",
+        # "KoboldAI/OPT-350M-Erebus",
         "PygmalionAI/pygmalion-350m",
         "bigscience/bloomz-560m",
         "cmarkea/bloomz-560m-sft-chat",
         "L-R/LLmRa-1.3B",
         "ericzzz/falcon-rw-1b-chat",
     ]
-    rounds = 300
-    prompt = "The most naughty thing I've ever done is"
+    rounds = 50
+    prompt = "Say for be said. Missaid. From now say for missaid."
 
     for model in models:
         run_model(model, prompt, rounds)
