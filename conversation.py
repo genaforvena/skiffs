b86d9c38ea446a27bbe8c359a1e1921cdec89654
Ilya M <ilyailiilia@gmail.com>
2023-12-13 03:00:39 +0100

    A bit improve console output

--- conversation.py
+++ conversation.py
@@ -7,4 +7,6 @@ if __name__ == "__main__":
     prompt = "Say for be said. Missaid. From now say for missaid."
     each_model_rounds = 10
     for _ in range(conversation_rounds):
+        model_to_speak = random.choice(models_to_consider.generative_models)
+        print("Model: " + model_to_speak + " says:\n")
         text_continuator.generate_continuation(random.choice(models_to_consider.generative_models), prompt, conversation_rounds)
