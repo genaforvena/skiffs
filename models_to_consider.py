87b0ea8bafde802e7548f9d9b29c3efb95fc0770
Ilya M <ilyailiilia@gmail.com>
2023-12-15 02:29:18 +0100

    Try the smallest models available

--- models_to_consider.py
+++ models_to_consider.py
@@ -1,24 +1,23 @@
 generative_models = [
-    "KoboldAI/OPT-350M-Erebus",
-    #"PygmalionAI/pygmalion-350m",
+    # "KoboldAI/OPT-350M-Erebus",
+    # "PygmalionAI/pygmalion-350m",
     # "bigscience/bloomz-560m",
     # "cmarkea/bloomz-560m-sft-chat",
     # "L-R/LLmRa-1.3B",
     # "ericzzz/falcon-rw-1b-chat",
     # "deepseek-ai/deepseek-coder-1.3b-instruct",
     "gpt2",
-    "distilgpt2",
+    # "distilgpt2",
+    "gpt-neo-125m",
     # "facebook/opt-1.3b",
-    #"HuggingFaceM4/tiny-random-LlamaForCausalLM",
-    #"EleutherAI/gpt-neo-125m",
-    #"facebook/opt-125M",
-    "facebook/opt-350m",
-    #"microsoft/phi-1"
- ]
-
-summarization_models = [
-    "sshleifer/distilbart-cnn-12-6"
+    # "HuggingFaceM4/tiny-random-LlamaForCausalLM",
+    # "EleutherAI/gpt-neo-125m",
+    "facebook/opt-125M",
+    # "facebook/opt-350m",
+    # "microsoft/phi-1"
 ]
+
+summarization_models = ["sshleifer/distilbart-cnn-12-6"]
 
 code_generation_models = [
     "gpt2",
