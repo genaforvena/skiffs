e59da913df45792f985f37c684714617d929c142
Ilya M <ilyailiilia@gmail.com>
2023-12-23 13:22:12 +0100

    Adjust feeder prompt, add first mememory

--- skiffs/feeder_producer.py
+++ skiffs/feeder_producer.py
@@ -1,4 +1,9 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    pipeline,
+    TextClassificationPipeline,
+)
 
 
 def prompt_injection_detector(text) -> str:
@@ -6,25 +11,47 @@ def prompt_injection_detector(text) -> str:
     tokenizer = AutoTokenizer.from_pretrained(select_model)
     model = AutoModelForSequenceClassification.from_pretrained(select_model)
     nlp = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-    return nlp(text)[0]["score"]
+    return nlp(text)[0]["label"]
 
 
-def prompt_generator(last_score) -> str:
-    prompt_generator = pipeline(
+def clinical_assertion(text):
+    tokenizer = AutoTokenizer.from_pretrained(
+        "bvanaken/clinical-assertion-negation-bert"
+    )
+    model = AutoModelForSequenceClassification.from_pretrained(
+        "bvanaken/clinical-assertion-negation-bert"
+    )
+    nlp = TextClassificationPipeline(model=model, tokenizer=tokenizer)
+    res = nlp(text)
+    for r in res:
+        if r["label"] == "POSSIBLE":
+            return r["score"]
+    return "0.0"
+
+
+def prompt_generator(prev_prompt, last_score) -> str:
+    generator = pipeline(
         task="text-generation",
         model="gpt2",
     )
-    return prompt_generator(
-        "The last score was "
+    instruction = (
+        "You've tried with "
+        + str(prev_prompt)
+        + ".\n"
+        + "The last score was "
         + str(last_score)
-        + ". Prompt to make the highest score possible:"
-    )[0]["generated_text"]
+        + ". \n"
+        + " Here is adjusted prompt to get the best score: "
+    )
+    return generator(instruction)[0]["generated_text"][len(instruction) :]
 
 
 if __name__ == "__main__":
     score = 0
+    prev_prompt = ""
     for i in range(100):
-        prompt = prompt_generator(score)
-        print(prompt)
-        score = prompt_injection_detector(prompt)
-        print(score)
+        prompt = prompt_generator(prev_prompt, score)
+        print("Prompt: " + prompt + "\n")
+        score = clinical_assertion(prompt)
+        print("Score:" + score + "\n")
+        prev_prompt = prompt
