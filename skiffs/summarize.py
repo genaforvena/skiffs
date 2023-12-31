bdc9fe8f9cb5658f1e803184f6533a02f5c916c3
Ilya M <ilyailiilia@gmail.com>
2023-12-31 05:40:51 +0100

    sightly improve summarization logging and change chunk division logic

--- skiffs/summarize.py
+++ skiffs/summarize.py
@@ -1,16 +1,35 @@
+import re
 from transformers import pipeline
 from datetime import datetime
+from typing import List
 
 from models import models_to_consider
 
 
-def divide_text(text):
+def divide_text(text: str, chunk_size: int = 256) -> List[str]:
     words = text.split()
     chunks = []
-    chunk_size = 300
-    for i in range(0, len(words), chunk_size):
-        chunk = words[i : i + chunk_size]
-        chunks.append(" ".join(chunk))
+    current_chunk = []
+    last_word_index = 0
+
+    for word in words:
+        current_chunk.append(word)
+        if word.endswith((".", "!", "?")):
+            last_word_index = len(current_chunk) - 1
+
+        if len(current_chunk) >= chunk_size:
+            if len(current_chunk) == chunk_size and current_chunk[-1].endswith(
+                (".", "!", "?")
+            ):
+                chunks.append(" ".join(current_chunk))
+                current_chunk = []
+            else:
+                chunks.append(" ".join(current_chunk[:last_word_index]))
+                current_chunk = current_chunk[last_word_index:]
+
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))
+
     return chunks
 
 
@@ -18,11 +37,15 @@ def summarize(txt: str, model_name: str) -> str:
     summarizer = pipeline("summarization", model=model_name)
     combined_summary = ""
     for chunk in divide_text(txt):
+        print("Summarizing: " + chunk)
         chunk_summary = summarizer(
             chunk, max_length=130, min_length=10, do_sample=False
         )[0]["summary_text"]
         combined_summary += chunk_summary + "\n\n"
-        print(chunk_summary)
+        print("\n\n")
+        print("Summary: " + chunk_summary)
+        print("\n\n")
+        print("\n\n")
 
     return combined_summary
 
