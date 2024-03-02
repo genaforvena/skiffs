d1a026a0b88f371f2b45cfd595e4d75ea65cab59
Ilya M <ilyailiilia@gmail.com>
2024-03-02 21:34:09 +0100

    Finnegannise not all words in the text, but only some to avoid repetitions in result

--- skiffs/util/finneganniser.py
+++ skiffs/util/finneganniser.py
@@ -1,18 +1,23 @@
 import random
 
 
+
 # Function to transform a sentence into a Finnegans Wake style sentence
 def finnegannise(text):
     words = text.split()
 
     transformed_words = []
     for word in words:
+        if random.random() < 0.85:
+            continue
+
         if len(word) > 3:
             new_word = word[:2] + random.choice(["ish", "esque", "ian"]) + word[2:]
         else:
             new_word = word
         if random.random() < 0.3:
             new_word = "".join(random.sample(new_word, len(new_word)))
+
 
         transformed_words.append(new_word)
 
