b9d4e56c48dc17ade186de7eeea4f7e498d25d74
Ilya M <ilyailiilia@gmail.com>
2024-03-02 17:57:25 +0100

    Add hallucinations to code

--- skiffs/summarize.py
+++ skiffs/summarize.py
@@ -103,6 +103,10 @@ class Summarizer:
             for i in range(checkpoint.current_chunk_index, len(text_chunks)):
                 current_chunk = text_chunks[i]
                 chunk_summary = self._summarize_chunk(current_chunk)
+                if self._hallucination_rounds_per_chunk > 0:
+                    chunk_summary = self._hallucinate_chunk(
+                        chunk_summary, self._hallucination_style
+                    )
                 chunk_summaries.append(chunk_summary)
                 _write_to_result(
                     self._log_file_name,
